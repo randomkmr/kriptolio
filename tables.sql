008ffcef84e2b5e544979004a9723aca0c79573e
kmr <karolis.lt@gmail.com>
2022-01-30 09:24:05 +0200

    DB: sql tables

---
+++ tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+id INT AUTO_INCREMENT PRIMARY KEY,
+full_name varchar(30),
+email varchar(80) UNIQUE NOT NULL,
+password varchar(80) NOT NULL,
+reg_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE favourites (
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+asset_code varchar(20) NOT NULL
+)
+
+CREATE TABLE portfolio (
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+asset_code varchar(20) NOT NULL,
+buying_price DECIMAL(10,2) NOT NULL,
+amount INT NOT NULL
+)
+
+CREATE TABLE descriptions (
+id INT AUTO_INCREMENT PRIMARY KEY,
+asset_code varchar(20) NOT NULL,
+description VARCHAR(200)
+)
